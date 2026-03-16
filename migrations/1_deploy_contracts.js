0cbc8aa025c360e53c4abc317547505a1972bb42
karim <u2597225@uel.ac.uk>
2026-03-16 23:15:32 +0000

    deploy EscrowFactory to Sepolia testnet - 0x91DC29AB63Ea6aa4d7dB084DB9b06c1Ff49d9196

--- migrations/1_deploy_contracts.js
+++ migrations/1_deploy_contracts.js
@@ -1,12 +1,19 @@
 const EscrowFactory = artifacts.require("EscrowFactory");
 
 module.exports = async function (deployer, network, accounts) {
-  const arbitrator = accounts[2];
-  
+  let arbitrator;
+
+  if (network === 'sepolia') {
+    // Use your own address as arbitrator on Sepolia
+    arbitrator = '0x654a7aa7B10D19640FCB4F5Cadf73c54ca6ab4C6';
+  } else {
+    arbitrator = accounts[2];
+  }
+
   console.log("Deploying with arbitrator:", arbitrator);
-  
+
   await deployer.deploy(EscrowFactory, arbitrator);
-  
+
   const factory = await EscrowFactory.deployed();
   console.log("EscrowFactory deployed at:", factory.address);
 };
--- truffle-config.js
+++ truffle-config.js
@@ -3,23 +3,24 @@ const HDWalletProvider = require('@truffle/hdwallet-provider');
 
 module.exports = {
   networks: {
-  development: {
-  host: "127.0.0.1",
-  port: 7545,
-  network_id: "5777",
-  gas: 6721975,
-  gasPrice: 20000000000,
-},
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "5777",
+      gas: 6721975,
+      gasPrice: 20000000000,
+    },
     sepolia: {
-      provider: () => new HDWalletProvider(
-        process.env.MNEMONIC,
-        `https://sepolia.infura.io/v3/${process.env.INFURA_PROJECT_ID}`
-      ),
-      network_id: 11155111,
-      confirmations: 2,
-      timeoutBlocks: 200,
-      skipDryRun: true
-    }
+  provider: () => new HDWalletProvider(
+    process.env.PRIVATE_KEY,
+    process.env.SEPOLIA_RPC
+  ),
+  network_id: 11155111,
+  confirmations: 2,
+  timeoutBlocks: 200,
+  skipDryRun: true,
+  networkCheckTimeout: 100000
+}
   },
 
   mocha: {
@@ -27,16 +28,15 @@ module.exports = {
   },
 
   compilers: {
-  solc: {
-    version: "0.8.21",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200
-      },
-      evmVersion: "london"
+    solc: {
+      version: "0.8.21",
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 200
+        },
+        evmVersion: "london"
+      }
     }
-  }
-
   }
 };
